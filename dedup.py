4aeb794055e4e0e482e13d4ddccc4f957ac133db
David Wallin <dwa@havanaclub.org>
2021-02-23 20:40:07 +0100

    changes++

--- dedup.py
+++ dedup.py
@@ -1,4 +1,5 @@
 from blake3 import blake3
+from xxhash import xxh3_128
 import click
 from click_help_colors import HelpColorsGroup, HelpColorsCommand
 import click_log
@@ -30,31 +31,10 @@ from IPython import embed
 def cli():
     pass
 
-# https://izziswift.com/get-md5-hash-of-big-files-in-python/
-# def md5_for_file(path, block_size=256*128, hex_result=False):
-#     '''
-#     Block size directly depends on the block size of your filesystem
-#     to avoid performances issues
-#     Here I have blocks of 4096 octets (Default NTFS)
-#     '''
-#     md5 = hashlib.md5()
-#     with open(path,'rb') as f:
-#         for chunk in iter(lambda: f.read(block_size), b''):
-#              md5.update(chunk)
-#     if hex_result:
-#         return md5.hexdigest()
-#     return md5.digest()
 
-
-# def blake3_for_file(path, block_size=256*128, hex_result=False):
-#     b3 = blake3()
-#     with open(path,'rb') as f:
-#         for chunk in iter(lambda: f.read(block_size), b''):
-#              b3.update(chunk)
-
-#     return b3.hexdigest() if hex_result else b3.digest()
-
-
+# FIXME: rewrite this, assume filepath is a Path object:
+# - filepath.open()
+# - check size: do a simple hashing if size < block_size
 def hash_file(filepath, block_size=65536, hex_result=True, hash_constructor=hashlib.md5):
     hasher = hash_constructor()
     with open(filepath,'rb') as f:
@@ -64,49 +44,34 @@ def hash_file(filepath, block_size=65536, hex_result=True, hash_constructor=hash
     return hasher.hexdigest() if hex_result else hasher.digest()
 
 
-# @cli.command()
-# @click.argument('filepath', type=click.Path(exists=True))
-# def list_dups(filepath):
-
-#     dups = defaultdict(list)
-
-#     p = Path(filepath).expanduser()
-#     ## FIXME: recurse
-#     p1 = p.glob('**/*')
-#     # FIXME: switch to tqdm?
-#     with click.progressbar(p1) as files:
-#         for f in files:
-#             if f.is_file() and not f.is_symlink():
-#                 #click.echo(f'file: {f}')
-#                 md5res = md5_for_file(f, block_size=65536, hex_result=True)
-#                 #click.echo(f'md5: {md5res}')
-#                 dups[md5res].append(f)
-#     d2 = {k: v for (k, v) in dups.items() if len(v) > 1}
-#     # FIXME: convert to pandas?
-#     embed()
-
-
+# FIXME: do what it says on the tin
 @cli.command()
 @click.argument('filepath', type=click.Path(exists=True))
 @click.option('-s', '--min-size', default='1', show_default=True)
-@click.option('-h', '--hash-type', type=click.Choice(['md5', 'blake3']), default='md5', show_default=True)
-#@click.option() # block-size (adaptive?)
+@click.option('-h', '--hash-type', type=click.Choice(['md5', 'blake3', 'xxh3']), default='md5', show_default=True)
+@click.option('-b', '--block-size', default='64K', show_default=True) # block-size (adaptive?)
 @click_log.simple_verbosity_option(logger)
-def list_dups_b3(filepath, min_size, hash_type):
+def list_dups(filepath, min_size, hash_type, block_size):
+
+    #filepath, min_size, hash_type
 
     logger.debug(f'hash-type: {hash_type}')
 
     hash_constructor = hashlib.md5
     if hash_type == 'blake3':
         hash_constructor = blake3
-
+    elif hash_type == 'xxh3':
+        hash_constructor = xxh3_128
 
     min_sz = parse_size(min_size)
+
+    blk_sz = parse_size(block_size, binary=True)
 
     dups = defaultdict(list)
     hardlinked = defaultdict(list)
 
     logger.debug(f'min-size: {min_sz}')
+    logger.debug(f'block-size: {blk_sz}')
 
     p = Path(filepath).expanduser()
 
@@ -116,21 +81,19 @@ def list_dups_b3(filepath, min_size, hash_type):
         if f.is_file() and not f.is_symlink():
             s = f.stat()
             if (s.st_size > min_sz):
-                logger.debug(f'processing: {f}')
-                # FIXME check if s.st_size > minsize
+                #logger.debug(f'processing: {f}')
                 if s.st_nlink > 1:
                     logger.debug(f'hardlinks [{s.st_nlink}]: file: {f}')
                     # Same Inodes:
                     hardlinked[(s.st_ino, s.st_dev)].append(f)
                 # FIXME: adaptive block_size??
-                hash_res = hash_file(f, block_size=65536, hex_result=True, hash_constructor=hash_constructor)
+                hash_res = hash_file(f, block_size=blk_sz, hex_result=True, hash_constructor=hash_constructor)
                 #click.echo(f'hash[{hash_type}]: {hash_res}')
                 dups[hash_res].append(f)
 
     d2 = {k: v for (k, v) in dups.items() if len(v) > 1}
     # FIXME: convert to pandas?
     embed()
-
 
 
 if __name__ == '__main__':
